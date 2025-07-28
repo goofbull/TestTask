09c3a9c9746682cb8ebf7622fb801fe48db85f73
goofbull <tordxie20@gmail.com>
2025-07-28 18:50:14 +0300

    chore: added comment lines

--- test/test.sh
+++ test/test.sh
@@ -2,14 +2,15 @@
 
 cd ..
 cd src
-python generate_file.py
-python server.py &
-sleep 1
-python client.py 
+python generate_file.py  # Запускаем генерацию файла со случайным наполнением
+python server.py &  # Запускаем сервер в фоновом режиме
+sleep 1             # Даем время на прогрузку
+python client.py    # Запускаем клиентскую программу
 
 file_to_send="random_file.txt"
 file_to_receive="received_file.txt"
 
+# Сравниваем сгенерированный файл и полученный
 if cmp -s "$file_to_send" "$file_to_receive"; then
     printf 'The file "%s" is the same as "%s"\n' "$file_to_send" "$file_to_receive"
 else
