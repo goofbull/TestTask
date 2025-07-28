dc7111ae05563289cad68f643fe03a6d84d105b4
goofbull <tordxie20@gmail.com>
2025-07-28 12:39:55 +0300

    feat: last type hint

--- src/generate_file.py
+++ src/generate_file.py
@@ -1,6 +1,7 @@
 import os
 
-fileSizeInBytes = 2*1024*1024  # Размер файла 2 МБ
+
+fileSizeInByte: int = 2*1024*1024  # Размер файла 2 МБ
 
 with open('random_file.txt', 'wb') as fout:  # Создаем файл случайного наполнения в размере 2 мегабайта
-    fout.write(os.urandom(fileSizeInBytes))
+    fout.write(os.urandom(fileSizeInByte))
