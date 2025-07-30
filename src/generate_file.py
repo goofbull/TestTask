8a4aa3d8357797c396f4e23b9c99acf35b28579c
goofbull <tordxie20@gmail.com>
2025-07-30 15:28:59 +0300

    chore: added input file size

--- src/generate_file.py
+++ src/generate_file.py
@@ -1,7 +1,6 @@
 import os
 
 
-fileSizeInByte: int = 2*1024*1024  # Размер файла 2 МБ
-
-with open('random_file.txt', 'wb') as fout:  # Создаем файл случайного наполнения в размере 2 мегабайта
-    fout.write(os.urandom(fileSizeInByte))
+def generate_file(fileSizeInByte: int) -> None:
+    with open('random_file.txt', 'wb') as fout:  # Создаем файл случайного наполнения в выбранном размере
+        fout.write(os.urandom(fileSizeInByte))
