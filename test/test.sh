c41ae0710050da747f9e8a0dc3c0ccc228850803
goofbull <tordxie20@gmail.com>
2025-07-30 15:30:07 +0300

    chore: added input file size

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
 
 cd ..
 cd src
-python generate_file.py  # Запускаем генерацию файла со случайным наполнением
-python server.py &  # Запускаем сервер в фоновом режиме
-sleep 1             # Даем время на прогрузку
-python client.py    # Запускаем клиентскую программу
+read -p "Input file size in bytes: " file_size  # Вписываем размер файла
+python server.py "$file_size" &  # Запускаем сервер в фоновом режиме, передаем размер файла
+sleep 2                  # Даем время на прогрузку
+python client.py         # Запускаем клиентскую программу, передаем размер файла
 
 file_to_send="random_file.txt"
 file_to_receive="received_file.txt"
 
 # Сравниваем сгенерированный файл и полученный
-if cmp -s "$file_to_send" "$file_to_receive"; then
+if diff "$file_to_send" "$file_to_receive"; then
     printf 'The file "%s" is the same as "%s"\n' "$file_to_send" "$file_to_receive"
 else
     printf 'The file "%s" is different from "%s"\n' "$file_to_send" "$file_to_receive"
