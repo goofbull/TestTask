23d3bcf493134e0b75a3dedac0c5dea4929c1c90
goofbull <tordxie20@gmail.com>
2025-07-30 12:25:08 +0300

    chore: added cmd input of file size, added type hints, added comments

--- src/client.py
+++ src/client.py
@@ -1,24 +1,36 @@
 import socket
+import sys
 
 
-def client_program():
+def client_program() -> None:
     # Создаем хост и порт
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Создание TCP сокета
+    sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Создание TCP сокета
     port: int = 5000  # Выбираем порт
-
+    
     print('connecting to server...')
     sock.connect(('localhost', port))  # Присоединение к серверу
 
-    filename: str = 'received_file.txt'
+    # Получаем размер файла из командной строки
+    if len(sys.argv) < 2:
+        print("Usage: python server.py <file_size>")
+        sys.exit(1)
+    try:
+        file_size: int = int(sys.argv[1])
+    except ValueError:
+        print("Error: file size must be an integer")
+        sys.exit(1)
 
+    filename: str = 'received_file.txt'
     with open(filename, 'wb') as file:
         print('the downloading has started')
-        data = sock.recv(2*1024*1024)  # Читаем 2 мегабайта
-        file.write(data)  # Записываем содержимое
+        while True:
+            data: bytes = sock.recv(file_size)  # Получаем содержимое файла
+            if not data:  # Если данных нет, выходим из цикла
+                break
+            file.write(data)  # Записываем содержимое
 
     file.close()  # Закрываем файл
     print('done downloading')
-
     sock.close()  # Завершаем сеанс
 
 if __name__ == '__main__':
--- src/server.py
+++ src/server.py
@@ -1,15 +1,32 @@
 import socket
+import sys
+from typing import Tuple
+from generate_file import generate_file
 
 
-def server_program():
+def server_program() -> None:
     # Создаем хост и порт
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Создание TCP сокета
+    sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Создание TCP сокета
     port: int = 5000  # Выбор порта
  
     sock.bind(('localhost', port))  # Привязка адреса хоста и порта
 
     sock.listen(2)  # Конфигурируем сколько всего клиентов смогут прослушивать сервер одновременно
-    
+
+    # Получаем размер файла из командной строки
+    if len(sys.argv) < 2:
+        print("Usage: python server.py <file_size>")
+        sys.exit(1)
+    try:
+        file_size: int = int(sys.argv[1])
+    except ValueError:
+        print("Error: file size must be an integer")
+        sys.exit(1)
+
+    generate_file(file_size)  # Генерируем файл со случайным наполнением
+
+    conn: socket.socket
+    address: Tuple[str, int]
     conn, address = sock.accept()  # Принимаем новое подключение
     print("connection from: " + str(address))
 
@@ -17,12 +34,13 @@ def server_program():
     
     with open(filename, 'rb') as file:
         print('opening the file...')
-        data = file.read(2*1024*1024)  # Читаем 2 мегабайта
+        data = file.read(file_size)  # Получаем файл
         conn.send(data)  # Отсылаем файл
 
     file.close()  # Закрываем файл
     print('the file has been sent')
-    conn.close()  # Завершаем сессию
+    sock.close()  # Закрываем сессию
+    conn.close()  # Отключаемся
 
 if __name__ == '__main__':
     server_program()
